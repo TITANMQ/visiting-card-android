df91fbb8d6ec51b846db2260eaa0d6e15a077af2
Elijah Rose <elijahr0001@gmail.com>
2018-11-04 20:58:51 +0000

    Add Google maps API key
    Fix issues
    
    add break statement

--- app/src/main/java/com/community/jboss/visitingcard/Maps/MapsActivity.java
+++ app/src/main/java/com/community/jboss/visitingcard/Maps/MapsActivity.java
@@ -1,17 +1,14 @@
 package com.community.jboss.visitingcard.Maps;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
+import android.os.Bundle;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.community.jboss.visitingcard.R;
 import com.community.jboss.visitingcard.VisitingCard.ViewVisitingCard;
-import com.community.jboss.visitingcard.VisitingCard.VisitingCardActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -58,8 +55,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng loacation = new LatLng(51.504407, -0.131642);
+        mMap.addMarker(new MarkerOptions().position(loacation).title("Marker in London"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(loacation));
+
     }
 }
